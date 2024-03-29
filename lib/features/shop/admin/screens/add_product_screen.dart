1b4b90867e0e98814afdc25a96a3dd99c263d643
NicholasNgaruiya <nicholasngaruiya387@gmail.com>
2024-03-29 18:41:20 +0300

    created the upload image ui

--- lib/features/shop/admin/screens/add_product_screen.dart
+++ lib/features/shop/admin/screens/add_product_screen.dart
@@ -5,9 +5,16 @@ import 'package:restaurant_frontend/utils/device/device_utility.dart';
 
 import '../../../../utils/constants/sizes.dart';
 
-class AddProductsPage extends StatelessWidget {
+class AddProductsPage extends StatefulWidget {
   const AddProductsPage({super.key});
 
+  @override
+  State<AddProductsPage> createState() => _AddProductsPageState();
+}
+
+class _AddProductsPageState extends State<AddProductsPage> {
+  String? dropDownValue;
+  var items = ['Drinks', 'Food', 'Snacks', 'Others'];
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,26 +23,117 @@ class AddProductsPage extends StatelessWidget {
           title: const Text('Add Product'),
           toolbarHeight: TDeviceUtils.getAppBarHeight(context),
         ),
-        body: Container(
-          padding: const EdgeInsets.all(8.0),
-          child: Padding(
+        body: SingleChildScrollView(
+          child: Container(
             padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const AddProductImage(),
-                const SizedBox(height: TSizes.spaceBtwSections),
-                Text(
-                  'Select Category',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const SelectCategorywidget(),
-                const SizedBox(height: TSizes.spaceBtwSections),
-                const Text(' Add Products'),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Add Image',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 10),
+                  const AddProductImage(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                  Text(
+                    'Select Category',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 10),
+                  const SelectCategorywidget(),
+                  const SizedBox(height: TSizes.spaceBtwSections),
+                  Text(
+                    'Product info',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                  const SizedBox(height: 10),
+                  Form(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              child: TextFormField(
+                                expands: false,
+                                decoration: const InputDecoration(
+                                  labelText: 'Product Name',
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: TSizes.spaceBtwInputFields),
+                            Expanded(
+                              child: TextFormField(
+                                expands: false,
+                                decoration: const InputDecoration(
+                                  labelText: 'Price',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+                        const SizedBox(height: TSizes.spaceBtwInputFields),
+                        TextFormField(
+                          maxLines: null,
+                          decoration: const InputDecoration(
+                            labelText: 'Product Description',
+                          ),
+                        ),
+                        const SizedBox(height: TSizes.spaceBtwItems),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 50,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors.red,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      'Clear',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              InkWell(
+                                onTap: () {},
+                                child: Container(
+                                  height: 50,
+                                  width: 100,
+                                  decoration: BoxDecoration(
+                                    color: Colors.green,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: const Center(
+                                    child: Text(
+                                      'Upload',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
